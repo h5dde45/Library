b7197fce4e564a3b31eb595af2bc04464d35044a
tmvf <tmvf@yandex.ru>
2017-07-19 17:32:20 +0300

    locale

---
+++ src/main/java/com/library/dao/impl/GenreDAOImpl.java
@@ -0,0 +1,23 @@
+package com.library.dao.impl;
+
+import com.library.dao.interfaces.GenreDAO;
+import com.library.entities.Genre;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class GenreDAOImpl implements GenreDAO{
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Genre> getGenres() {
+        return sessionFactory.getCurrentSession().createCriteria(Genre.class).list();
+    }
+}
---
+++ src/main/java/com/library/dao/interfaces/GenreDAO.java
@@ -0,0 +1,10 @@
+package com.library.dao.interfaces;
+
+import com.library.entities.Genre;
+import java.util.List;
+
+public interface GenreDAO {
+
+    List<Genre> getGenres();
+
+}
